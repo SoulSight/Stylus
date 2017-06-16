a9a41465933d2abd3556dd6e008abe81c371ab75
SoulSight <soulsight@163.com>
2017-06-16 22:14:07 +0800

    Create reasoning stub

---
+++ src/Stylus/Stylus/Reasoning/Rule.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stylus.Reasoning
+{
+    public class RuleHead
+    {
+
+    }
+
+    public class RuleBody
+    {
+
+    }
+
+    public abstract class Rule
+    {
+        public RuleHead Head { set; get; }
+
+        public RuleBody Body { set; get; }
+
+        public abstract void Apply();
+    }
+}
---
+++ src/Stylus/Stylus/Reasoning/RuleNode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stylus.Reasoning
+{
+    public class RuleNode { }
+
+    public class RuleLiteralNode : RuleNode { }
+
+    public class RuleVariableNode : RuleNode { }
+
+    public class RuleExpr : RuleNode { }
+
+    public class RuleTree : RuleNode { }
+}
