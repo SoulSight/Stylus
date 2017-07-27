067d4b5492316fa6fcca4540fd1870d491b0c957
SoulSight <soulsight@163.com>
2017-07-27 15:38:02 +0800

    Rename Query Files & Add Rule Types

---
+++ src/Stylus/Stylus/Reasoning/RuleType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stylus.Reasoning
+{
+    public enum RuleType
+    {
+        TBox, 
+        ABoxTransitive,
+        ABoxNonTransitive
+    }
+}
